eefb43f053eafa368084be9c6f2fe42fe38c096f
Lydia De Castro Sousa <lydiadecastrosousa@gmail.com>
2022-03-16 12:24:09 +0100

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,59 +1,61 @@
-//j'importe le package http de node
-const http = require('http');
+//Import du framework express
+const express = require('express');
 
-//j'importe l'application
-const app = require('./app');
+require('dotenv').config({ path: './config/.env' })
 
+//j'importe mysql
+const mysql = require('mysql');
 
-//trouve un port valide
-const normalizePort = val => {
-    const port = parseInt(val, 10);
-
-    if (isNaN(port)) {
-        return val;
-    }
-    if (port >= 0) {
-        return port;
-    }
-    return false;
-};
-
-//j'indique à l'app express sur quel port elle doit tourner
-const port = normalizePort(process.env.PORT ||  '3000');
-app.set('port', port);
-
-
-//recherche les erreurs
-const errorHandler = error => {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
-    const address = server.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges.');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use.');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-};
-
-//je crée mon serveur
-const server = http.createServer(app);
-//gestion d'erreur
-server.on('error', errorHandler);
-//ecoute du port d'execution du serveur
-server.on('listening', () => {
-    const address = server.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-    console.log('Listening on ' + bind);
+//créer connexion
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'lydiaDCS24!',
+    database: 'nodemysql'
 });
 
-//Ecoute des requêtes envoyées par le port disponible
-server.listen(port);
+//Connecter
+db.connect((err) => {
+    if (err) {
+        return err;
+    }
+    console.log('Connecté à la base de données MySQL!');
+    db.query("CREATE DATABASE nodemysql", function(err, result) {
+        if (err) throw err;
+        console.log("Base de données créée !");
+    });
+
+});
+
+//je declare app pour express
+const app = express();
+
+//Creation de la BDD à travers une route get //middleware
+app.get('/createdb', (req, res) => {
+    //requête
+    let sql = 'CREATE DATABASE nodemysql';
+    db.query(sql, (err, result) => {
+        if (err) {
+            throw err;
+        }
+        console.log(result);
+        res.send('database created...');
+
+    });
+});
+
+//Créer une table
+app.get('/createpoststable', (req, res) => {
+    let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY id)';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Posts table create...');
+
+    });
+});
+
+//Port
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+})
